e81d18b4c1fa72a62b4b2ce06de2a02aa84f7be5
Saurabh Ojha <saurabhojha@Saurabhs-MacBook-Pro.local>
2021-02-25 16:16:52 +0530

    initial commit

---
+++ src/main/java/com/saurabh/springmongosecuritydemo/SpringMongoSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.springmongosecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoSecurityDemoApplication.class, args);
+	}
+
+}
